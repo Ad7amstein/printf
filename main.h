02752a4165606fb5b3364aec7fc123214335d8bf
3llam <adham32003200@gmail.com>
2023-04-23 01:01:49 +0200

    modify style

--- main.h
+++ main.h
@@ -4,16 +4,23 @@
 #include <stdio.h>
 #include <stdarg.h>
 #include <unistd.h>
-typedef struct format 
+#include <stdlib.h>
+/**
+ * struct format - match and convert the specifiers for printf
+ *
+ * @c: specifier
+ * @f: function to be executed
+ */
+typedef struct format
 {
 	char c;
 	int (*f)();
 } convert_specifier;
 int _putchar(char c);
 int _puts(char *str);
-int _printf(const char *format, ...);
 int print_char(va_list x);
 int print_string(va_list x);
 int print_percent_sign(void);
+int _printf(const char *format, ...);
 
 #endif /* MAIN_H */
