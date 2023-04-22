70f7f85987829ed03fc523c44c96dacf8c1c2622
3llam <adham32003200@gmail.com>
2023-04-23 00:07:04 +0200

    add function to print one letter

---
+++ _putchar.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _putchar - fuction to print one letter
+ *
+ * @c: letter to be printed
+ *
+ * Return: int - 1 (Success),
+ * -1 (Failed)
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
