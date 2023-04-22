0e229c8d7b4278c2daf834d55234c7ad0f2f3167
3llam <adham32003200@gmail.com>
2023-04-23 00:54:43 +0200

    print strings

---
+++ print_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_string - function to print string
+ *
+ * @x: char to be printed
+ * Return: int
+ */
+int print_string(va_list x)
+{
+	char *s;
+	int i;
+
+	i = 0;
+	s = va_arg(x, char *);
+	if (s == NULL)
+		s = "(null)";
+	while (s[i])
+	{
+		_putchar(s[i]);
+		i++;
+	}
+
+	return (i);
+}
