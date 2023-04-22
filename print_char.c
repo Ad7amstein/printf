dd7cff7ff747f44040acc0c93700849533af9120
3llam <adham32003200@gmail.com>
2023-04-23 00:54:22 +0200

    print one letter

---
+++ print_char.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_char - function to print char
+ *
+ * @x: char to be printed
+ * Return: int
+ */
+int print_char(va_list x)
+{
+	char c;
+	c = (char)va_arg(x, int);
+	_putchar(c);
+	return (1);
+}
