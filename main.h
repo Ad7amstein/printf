46c48d5f8c1062a45764e5701a218d67183ee0a1
3llam <adham32003200@gmail.com>
2023-04-23 00:09:14 +0200

    add header file

---
+++ main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <limits.h>
+#include <stdio.h>
+#include <stdarg.h>
+#include <unistd.h>
+int _putchar(char c);
+int _puts(char *str);
+int _printf(const char *format, ...);
+
+#endif /* MAIN_H */
