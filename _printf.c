9a210de140fa48304c0fd6abda1f2583e5de91c8
3llam <adham32003200@gmail.com>
2023-04-23 16:25:44 +0200

    Updated

--- _printf.c
+++ _printf.c
@@ -21,6 +21,8 @@ int _printf(const char *format, ...)
 	i = 0;
 	count = 0;
 	va_start(args, format);
+	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
+		return (-1);
 	while (format && format[i])
 	{
 		if (format[i] == '%')
