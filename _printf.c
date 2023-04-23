251b43f3ec2c2ccd827e8211a95951ae709ac680
3llam <adham32003200@gmail.com>
2023-04-23 16:36:24 +0200

    Updated

--- _printf.c
+++ _printf.c
@@ -28,6 +28,8 @@ int _printf(const char *format, ...)
 		if (format[i] == '%')
 		{
 			i++;
+			if (format[i] == '\0')
+				break;
 			j = 0;
 			while (matches[j].c != 'n' && matches[j].f != NULL)
 			{
