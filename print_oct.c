b4cfaf294a767d91635702647a32fa644d0ed796
3llam <adham32003200@gmail.com>
2023-04-24 23:04:43 +0200

    print integer in octal notation

---
+++ print_oct.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_oct - print unsigned int argument converted to octal
+ *
+ * @n: number to be converted and printed
+ * Return: int - number of characters printed
+ */
+int print_oct(va_list n)
+{
+	unsigned int num, i, len;
+	char *str;
+
+	num = va_arg(n, unsigned int);
+
+	str = convert(num, 8);
+
+	i = 0;
+	while (str[i])
+	{
+		_putchar(str[i++]);
+		len++;
+	}
+
+	free(str);
+	return (len);
+}
