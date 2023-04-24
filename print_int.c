a17e374da33f9834e49bea363c51ed3609ce93ee
3llam <adham32003200@gmail.com>
2023-04-24 23:03:25 +0200

    print unsigned integer

---
+++ print_int.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_ui - print unsigned int argument
+ *
+ * @n: number to be converted and printed
+ * Return: int - number of characters printed
+ */
+int print_ui(va_list n)
+{
+	unsigned int num, i, len;
+	char *str;
+
+	num = va_arg(n, unsigned int);
+
+	str = convert(num, 10);
+
+	i = 0;
+	while (str[i])
+	{
+		_putchar(str[i++]);
+		len++;
+	}
+
+	free(str);
+	return (len);
+}
