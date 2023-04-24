1676df1f56b3a31abc27ec56bdb34e309e306a3c
3llam <adham32003200@gmail.com>
2023-04-24 23:05:49 +0200

    print printable letters with custom specifier 'S'

---
+++ print_printable_letters.c
@@ -0,0 +1,39 @@
+#include "main.h"
+/**
+ * print_printable_letters - print printable letters only
+ *
+ * @arg: string to be printed
+ * Return: int - number of characters printed
+ */
+int print_printable_letters(va_list arg)
+{
+	char *s;
+	unsigned int i, num, len;
+
+	s = va_arg(arg, char *);
+	if (s == NULL)
+		s = "(null)";
+	i = 0;
+	while (s[i])
+	{
+		if ((0 < s[i] && s[i] < 32) || s[i] >= 127)
+		{
+			_putchar('\\');
+			_putchar('x');
+			num = (unsigned int)s[i];
+			if (num < 16)
+				_putchar('0');
+			print_2digits_HEX(num);
+			len += 4;
+		}
+		else
+		{
+			len++;
+			_putchar(s[i]);
+		}
+
+		i++;
+	}
+
+	return (len);
+}
