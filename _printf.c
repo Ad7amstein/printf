2511ccf9450e82bd076e78afe78f1d3a8fa6115a
abd el-fattah <abdelafatahwenny@yahoo.com>
2023-04-25 18:04:46 -0700

    added one more condition to prevent null only

--- _printf.c
+++ _printf.c
@@ -20,7 +20,7 @@ int _printf(const char *format, ...)
 		return (-1);
 	if (format[0] == '%' && format[1] == ' ' && !format[2])
 		return (-1);
-	while (format[i])
+	while (format[i] != '\0')
 	{
 		if (format[i] == '%')
 		{
