27aa47ef6a37085eefecfa129927004864efb8d6
abd el-fattah <abdelafatahwenny@yahoo.com>
2023-04-25 18:33:10 -0700

    restore the past changes

--- _printf.c
+++ _printf.c
@@ -16,7 +16,7 @@ int _printf(const char *format, ...)
 	i = 0;
 	count = 0;
 	va_start(args, format);
-	if (format == NULL || format == 0 || (format[0] == '%' && format[1]))
+	if (format == NULL || format == 0 || (format[0] == '%' && format[1] == '\0'))
 		return (-1);
 	if (format[0] == '%' && format[1] == ' ' && !format[2])
 		return (-1);
